211c63b742440d4cdcfb29323a32d243a4857d69
Jenni Choi <jenni0729@gmail.com>
2015-03-30 14:51:53 -0700

    change to models

--- giftbox/app/models/item.rb
+++ giftbox/app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  belongs_to :user
+
+  validates :product, :give_quantity, :want_quantity, presence: true
 end
--- giftbox/app/models/user.rb
+++ giftbox/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+  
+  has_many :items
+  
+  validates :username, :email, uniqueness: true
+  validates :password, length: {minimum: 4}
+  validates :first_name, :last_name, :phone, :address, presence: true
 end
