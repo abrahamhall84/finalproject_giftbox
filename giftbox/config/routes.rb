dbe8839abf14aa4c0a6b571bc86c221fd6c9642e
Abraham Hall <abrahamhall84@gmail.com>
2015-03-30 14:44:07 -0700

    made the items and users controllers

--- giftbox/config/routes.rb
+++ giftbox/config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'items/index'
+
+  get 'items/new'
+
+  get 'items/show'
+
+  get 'items/edit'
+
+  get 'homepage/index'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
